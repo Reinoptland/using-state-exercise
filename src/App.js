064b71129800d3d8cdf9f04daa915689cbc31ee5
Reinoptland <reinopland@gmail.com>
2021-03-12 09:20:02 +0100

    Move score state and event listeners & handlers to TeamScore

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import TeamScore from "./components/TeamScore";
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [score, setScore] = useState(0);
-
-  function changeScore(number) {
-    setScore(score + number);
-  }
-
-  function halfScore() {
-    setScore(score / 2);
-  }
-
-  function resetScore() {
-    setScore(0);
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>ScoreBoard</h1>
-        <div>
-          <div>
-            <p class="counter counter--blue">Team blue: {score}</p>
-          </div>
-          <div class="buttons">
-            <button onClick={() => changeScore(1)}>+ 1</button>
-            <button onClick={() => changeScore(-1)}>- 1</button>
-            <button onClick={() => changeScore(5)}>+ 5</button>
-            <button onClick={() => changeScore(-5)}>- 5</button>
-            <button onClick={halfScore}>half score</button>
-            <button onClick={resetScore}>reset</button>
-          </div>
-        </div>
+        <TeamScore teamColor="blue" />
         <TeamScore teamColor="red" />
         <TeamScore teamColor="pink" />
       </header>
--- src/components/TeamScore.js
+++ src/components/TeamScore.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TeamScore(props) {
+  const [score, setScore] = useState(0);
+
+  function changeScore(number) {
+    setScore(score + number);
+  }
+
+  function halfScore() {
+    setScore(score / 2);
+  }
+
+  function resetScore() {
+    setScore(0);
+  }
   return (
     <div>
       <div>
-        <p class={`counter counter--${props.teamColor}`}>
-          Team {props.teamColor}: 0
+        <p className={`counter counter--${props.teamColor}`}>
+          Team {props.teamColor}: {score}
         </p>
       </div>
-      <div class="buttons">
-        <button>+ 1</button>
-        <button>- 1</button>
-        <button>+ 5</button>
-        <button>- 5</button>
-        <button>half score</button>
-        <button>reset</button>
+      <div className="buttons">
+        <button onClick={() => changeScore(1)}>+ 1</button>
+        <button onClick={() => changeScore(-1)}>- 1</button>
+        <button onClick={() => changeScore(5)}>+ 5</button>
+        <button onClick={() => changeScore(-5)}>- 5</button>
+        <button onClick={halfScore}>half score</button>
+        <button onClick={resetScore}>reset</button>
       </div>
     </div>
   );
